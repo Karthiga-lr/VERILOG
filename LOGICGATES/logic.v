ae99967414d700429030dfdf015b0d88c5aa5dda
Karthiga-lr <lr.karthiga20@gmail.com>
2025-05-21 10:30:18 +0530

    Update logic.v

--- LOGICGATES/logic.v
+++ LOGICGATES/logic.v
@@ -1,1 +1,11 @@
-
+module basicgate(a,b,cand,cor,cnot,cnand,cnor,cxor,cxnor);
+  input a,b;
+  output cand,cor,cnot,cnand,cnor,cxor,cxnor;
+  and a1(cand,a,b);
+  or a2(cor,a,b);
+  not a3(cnot,a);
+  nand a4(cnand,a,b);
+  nor a5(cnor,a,b);
+  xor a6(cxor,a,b);
+  xnor a7(cxnor,a,b);
+endmodule
