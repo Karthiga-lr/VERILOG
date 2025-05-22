360c9dd8c04a16caad96ac6a81cfb3b0a9d9d47b
Karthiga-lr <lr.karthiga20@gmail.com>
2025-05-22 18:26:20 +0530

    Update binarytogray_tb.v

--- DAY-3/COMBINATIONAL CIRCUITS-PART 2/CONVERTERS/binarytogray_tb.v
+++ DAY-3/COMBINATIONAL CIRCUITS-PART 2/CONVERTERS/binarytogray_tb.v
@@ -1,1 +1,18 @@
-
+module binarytogray_convtb;
+  reg b2,b1,b0;
+  wire g2,g1,g0;
+  binarytogray_conv duv(.b2(b2),.b1(b1),.b0(b0),.g2(g2),.g1(g1),.g0(g0));
+                   initial
+                     begin
+                       $dumpfile("test.vcd");
+                       $dumpvars;
+                       b2=0;b1=0;b0=0;#10;
+                       b2=0;b1=0;b0=1;#10;
+                       b2=0;b1=1;b0=0;#10;
+                       b2=0;b1=1;b0=1;#10;
+                       b2=1;b1=0;b0=0;#10;
+                       b2=1;b1=0;b0=1;#10;
+                       b2=1;b1=1;b0=0;#10;
+                       b2=1;b1=1;b0=1;#10;
+                     end
+                   endmodule
