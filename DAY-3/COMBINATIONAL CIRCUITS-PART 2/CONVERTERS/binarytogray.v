5e8525fdfdb14a9ddf6f4acab2abed47b4c8ab54
Karthiga-lr <lr.karthiga20@gmail.com>
2025-05-22 18:25:19 +0530

    Update binarytogray.v

--- DAY-3/COMBINATIONAL CIRCUITS-PART 2/CONVERTERS/binarytogray.v
+++ DAY-3/COMBINATIONAL CIRCUITS-PART 2/CONVERTERS/binarytogray.v
@@ -1,1 +1,5 @@
-
+module binarytogray_conv(input b2,b1,b0, output g2,g1,g0);
+                    assign g0=b1^b0;
+                    assign g1=b2^b1;
+                    assign g2=b2;
+                    endmodule
