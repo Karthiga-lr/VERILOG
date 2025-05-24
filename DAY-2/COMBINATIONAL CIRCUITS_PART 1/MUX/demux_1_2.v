283563ba344186478799f124aaee4caa6796be1f
Karthiga-lr <lr.karthiga20@gmail.com>
2025-05-24 21:16:40 +0530

    Delete DAY-2/COMBINATIONAL CIRCUITS_PART 1/MUX directory

--- DAY-2/COMBINATIONAL CIRCUITS_PART 1/MUX/demux_1_2.v
+++
@@ -1,6 +0,0 @@
-module demux_1_2(s,a,yo,yi);
-  input s,a;
-  output yo,yi;
-  assign yo=~s&a;
-  assign yi=s&a;
-endmodule
--- DAY-2/COMBINATIONAL CIRCUITS_PART 1/MUX/mux2_1.v
+++
@@ -1,3 +0,0 @@
-module mux_2_1(input a,b,s, output y);
-  assign y=(a&~s)|(b&s);
-endmodule
