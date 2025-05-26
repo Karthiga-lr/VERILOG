c048be5b2b7e2fbcb70ef5e332b11e42c8364896
Karthiga-lr <lr.karthiga20@gmail.com>
2025-05-26 21:00:56 +0530

    Delete Day-5- SEQUENTIAL CIRCUITS_PART1/LATCHES directory

--- Day-5- SEQUENTIAL CIRCUITS_PART1/LATCHES/d.latch.v
+++
@@ -1,4 +0,0 @@
-module d(input d,output q,qb);
-  assign q = ~(qb|d);
-  assign qb = ~(q|(~d));
-endmodule
--- Day-5- SEQUENTIAL CIRCUITS_PART1/LATCHES/sr_latch.v
+++
@@ -1,5 +0,0 @@
-//Data flow model
-module sr(input s,r, output q,qb);
-  assign q = ~(r | qb);
-  assign qb = ~(s|q);
-endmodule
