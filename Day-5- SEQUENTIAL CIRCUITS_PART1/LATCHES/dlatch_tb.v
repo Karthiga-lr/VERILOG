cf088be12081fc9905cefdabca6e95b52490ac4e
Karthiga-lr <lr.karthiga20@gmail.com>
2025-05-26 21:00:36 +0530

    Create dlatch_tb.v

---
+++ Day-5- SEQUENTIAL CIRCUITS_PART1/LATCHES/dlatch_tb.v
@@ -0,0 +1,12 @@
+module d_tb();
+  reg d;
+  wire q,qb;
+  d duv(.d(d),.q(q),.qb(qb));
+  initial begin
+    $dumpfile("test.vcd");
+    $dumpvars;   
+    $monitor($time,"d=%b,q=%b,qb=%b",d,q,qb);
+    d=0;#10;
+    d=1;#10;
+  end
+endmodule
