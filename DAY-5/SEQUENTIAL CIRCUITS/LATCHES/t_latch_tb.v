27bc94a99f07e85ed2a49b01a7299cf781071458
Karthiga-lr <lr.karthiga20@gmail.com>
2025-05-27 10:34:38 +0530

    Update t_latch_tb.v

--- DAY-5/SEQUENTIAL CIRCUITS/LATCHES/t_latch_tb.v
+++ DAY-5/SEQUENTIAL CIRCUITS/LATCHES/t_latch_tb.v
@@ -1,1 +1,10 @@
-
+module tlatch_tb();
+  reg t;
+  wire q,qb;
+  tlatch duv(.t(t),.q(q),.qb(qb));
+  initial begin
+    $monitor($time,"t=%b,q=%b,qb=%b",t,q,qb);
+    t=0;#10;
+    t=1;#10;
+  end
+endmodule
