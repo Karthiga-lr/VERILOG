f00a4e1e4989607575377cff9f7af8d5fe71ab76
Karthiga-lr <lr.karthiga20@gmail.com>
2025-05-27 09:59:09 +0530

    Update Jk_latch_tb.v

--- DAY-5/SEQUENTIAL CIRCUITS/LATCHES/Jk_latch_tb.v
+++ DAY-5/SEQUENTIAL CIRCUITS/LATCHES/Jk_latch_tb.v
@@ -1,1 +1,13 @@
-
+module jk_tb();
+  reg j,k;
+  wire q,qb;
+  jk duv(.j(j),.k(k),.q(q),.qb(qb));
+  initial begin
+    
+    $monitor($time,"j=%b,k=%b,q=%b,qb=%b",j,k,q,qb);
+    j=0;k=0;#10;
+    j=0;k=1;#10;
+    j=1;k=0;#10;
+    j=1;k=1;#10;
+  end
+endmodule
