4aa718884f1fb0a6dedd8de006d4770a9d89ad6b
Karthiga-lr <lr.karthiga20@gmail.com>
2025-05-28 11:19:42 +0530

    Create up_counter.v

---
+++ DAY-5/SEQUENTIAL CIRCUITS/COUNTERS/up_counter.v
@@ -0,0 +1,9 @@
+module upcount(input rst,clk, output reg [3:0] count);
+  always@(posedge clk)
+    begin
+      if(rst)
+        count<=4'b0000;
+      else
+        count<=count+1;
+    end 
+endmodule
