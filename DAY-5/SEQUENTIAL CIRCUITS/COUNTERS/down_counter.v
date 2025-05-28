5aecedf4136c590eea4aad7bd688c72597e005f4
Karthiga-lr <lr.karthiga20@gmail.com>
2025-05-28 11:23:04 +0530

    Create down_counter.v

---
+++ DAY-5/SEQUENTIAL CIRCUITS/COUNTERS/down_counter.v
@@ -0,0 +1,9 @@
+module downcount(input rst,clk, output reg [3:0] count);
+  always@(posedge clk)
+    begin
+      if(rst)
+        count<=4'b0000;
+      else
+        count<=count-1;
+    end 
+endmodule
