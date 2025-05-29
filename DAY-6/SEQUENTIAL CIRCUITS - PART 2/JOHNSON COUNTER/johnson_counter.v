2c0fe44790475946dcb4fe90cdb58834a0a86370
Karthiga-lr <lr.karthiga20@gmail.com>
2025-05-29 12:24:40 +0530

    Update johnson_counter.v

--- DAY-6/SEQUENTIAL CIRCUITS - PART 2/JOHNSON COUNTER/johnson_counter.v
+++ DAY-6/SEQUENTIAL CIRCUITS - PART 2/JOHNSON COUNTER/johnson_counter.v
@@ -1,1 +1,18 @@
+module johnsoncount(
+  input clk,
+  input rst,
+  output reg [3:0] q
+);
+
+  always @(posedge clk) begin
+    if (rst==1)
+      q <= 4'b0000; 
+    else begin
+      q[0] <= ~q[3]; //Invert msb and store in lsb
+      q[1] <= q[0];
+      q[2] <= q[1];
+      q[3] <= q[2];
+    end
+  end
+endmodule
 
