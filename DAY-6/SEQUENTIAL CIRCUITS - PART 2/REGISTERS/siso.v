0a7d2149ab921b218618e074e54605a707292ab6
Karthiga-lr <lr.karthiga20@gmail.com>
2025-05-29 13:21:30 +0530

    Update siso.v

--- DAY-6/SEQUENTIAL CIRCUITS - PART 2/REGISTERS/siso.v
+++ DAY-6/SEQUENTIAL CIRCUITS - PART 2/REGISTERS/siso.v
@@ -1,1 +1,15 @@
-
+module siso(si,clk,rst,so);
+  input si,clk,rst;
+  output reg so;
+  reg [3:0] temp;
+  always@(posedge clk)begin
+    if (rst==1)begin
+      temp=4'b0;
+  end
+  else begin
+    temp=temp>>1;
+    temp[3]<=si;
+    so<=temp[0];
+  end
+  end
+endmodule
