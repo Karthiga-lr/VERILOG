d7e93b0d5dd5be6965624408fbc0fd8bfc0109af
Karthiga-lr <lr.karthiga20@gmail.com>
2025-05-29 12:18:28 +0530

    Update ring_counter.v

--- DAY-6/SEQUENTIAL CIRCUITS - PART 2/RING COUNTER/ring_counter.v
+++ DAY-6/SEQUENTIAL CIRCUITS - PART 2/RING COUNTER/ring_counter.v
@@ -1,1 +1,13 @@
+module ringcount(input clk,input rst,output reg [3:0] q);
+  reg [3:0] a;
+  always @(posedge clk) begin
+    if (rst==1)
+      a <= 4'b0001;
+    else
+      a <= a<<1;
+  end
 
+  always @(*) begin
+    q = a;
+  end
+endmodule
