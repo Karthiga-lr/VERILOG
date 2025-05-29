884a4350f47822267db9c87abd5025888791c778
Karthiga-lr <lr.karthiga20@gmail.com>
2025-05-29 12:25:52 +0530

    Create johnson_counter_tb.v

---
+++ DAY-6/SEQUENTIAL CIRCUITS - PART 2/JOHNSON COUNTER/johnson_counter_tb.v
@@ -0,0 +1,20 @@
+module johnsoncount_tb;
+  reg clk, rst;
+  wire [3:0] q;
+
+johnsoncount duv (.clk(clk),.rst(rst),.q(q));
+  initial begin
+    clk = 0;
+    forever #10 clk = ~clk;
+  end
+  initial begin
+    $dumpfile("test.vcd");
+    $dumpvars(1);
+    $monitor("time=%0t clk=%b rst=%b q=%b", $time, clk, rst, q);
+
+    rst = 0;
+    #20 rst = 1;
+    #20 rst = 0;
+    #100 $finish;
+  end
+endmodule 
