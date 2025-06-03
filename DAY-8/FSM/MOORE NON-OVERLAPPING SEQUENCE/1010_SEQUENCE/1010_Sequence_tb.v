4fc12b98d0979c9ff6b3f48f2bb54afe9c95b8b6
Karthiga-lr <lr.karthiga20@gmail.com>
2025-06-03 12:58:33 +0530

    Create 1010_Sequence_tb.v

---
+++ DAY-8/FSM/MOORE NON-OVERLAPPING SEQUENCE/1010_SEQUENCE/1010_Sequence_tb.v
@@ -0,0 +1,24 @@
+ module moore_1010detec_tb;
+  reg clk,rst,in;
+  wire out;
+moore_1010detec dut(.clk(clk),.rst(rst),.in(in),.out(out));
+                  initial clk=0;
+                  always#5 clk=~clk; 
+                  initial begin
+                    $dumpfile("test.vcd");
+                    $dumpvars(1);
+                  end
+                  initial begin
+                    in=0;
+                    #10 rst=0;
+                    #10 rst=1;
+                    
+                    #10 in=1;
+                    #10 in=0;
+                    #10 in=1;
+                    #10 in=0;
+                    
+                    #50 $finish;
+                  end
+                  endmodule
+                  
