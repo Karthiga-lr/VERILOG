9a0d84faea642c24a74fb8a9913624d48000d4e6
Karthiga-lr <lr.karthiga20@gmail.com>
2025-06-03 16:47:00 +0530

    Update 1010_sequence_tb.v

--- DAY-7/FSM/MEALEY OVERLAPPING SEQUENCE/1010_SEQUENCE/1010_sequence_tb.v
+++ DAY-7/FSM/MEALEY OVERLAPPING SEQUENCE/1010_SEQUENCE/1010_sequence_tb.v
@@ -1,26 +1,30 @@
 module sequedetec_1010_tb;
-  reg clk,rst,in;
+  reg clk, rst, in;
   wire out;
-  sequedetec_1010 duv(.clk(clk),.rst(rst),.in(in),.out(out));
-  initial clk=0;
-  always#5clk=~clk;
+  
+  sequedetec_1010 duv(.clk(clk), .rst(rst), .in(in), .out(out));
+
+  initial clk = 0;
+  always #5 clk = ~clk;
   initial begin
     $dumpfile("test.vcd");
-    $dumpvars;
+    $dumpvars(0, sequedetec_1010_tb);
   end
-    initial begin
-      in=0;
-      #10 rst=0;
-      #10 rst=1;
-      
-      #10 in=1;
-      #10 in=0;
-      #10 in=1;
-      #10 in=0;
-      #10 in=1;
-      
-      #40$finish;
-    end
+
+  initial begin
+    rst = 0; in = 0;
+    #10 rst = 1;
+
+    #10 in = 1;  
+    #10 in = 0;  
+    #10 in = 1;  
+    #10 in = 0;  
+    #10 in = 1;
+    #40 $finish;
+  end
+
 endmodule
+
      
+      
       
