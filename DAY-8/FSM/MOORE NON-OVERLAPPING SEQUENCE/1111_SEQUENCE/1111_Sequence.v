1026c324746c0319c2871449e83700f6bf254678
Karthiga-lr <lr.karthiga20@gmail.com>
2025-06-03 12:00:55 +0530

    Update and rename 1111_Sequence to 1111_Sequence.v

---
+++ DAY-8/FSM/MOORE NON-OVERLAPPING SEQUENCE/1111_SEQUENCE/1111_Sequence.v
@@ -0,0 +1,58 @@
+module moore_1111detec(
+    input clk,
+    input rst,
+    input in,
+    output reg out
+);
+    parameter s0 = 2'd0,s1 = 2'd1,s10 = 2'd2,s101 = 2'd3,s1011=2'd3;
+    reg [1:0] state, next_state;
+  always @(posedge clk or posedge rst) begin
+      if (rst!=1)
+            state <= s0;
+        else
+            state <= next_state;
+    end
+
+    always @(*) begin
+      if (state == s0) begin
+          if (in==1)
+                next_state = s1;
+            else
+                next_state = s0;
+        end
+      else if (state == s1) begin
+        if (in==1)
+                next_state = s10;
+            else
+                next_state = s0;
+        end
+      else if (state == s10) begin
+        if (in==1)
+                next_state = s101;
+            else
+                next_state = s0;
+        end
+      else if (state == s101) begin
+        if (in==1)
+                next_state = s1011;
+            else
+                next_state = s10;
+        end
+      else if (state == s1011) begin
+        if (in==1)
+                next_state = s1;
+            else
+                next_state = s0;
+        end
+        else begin
+            next_state = s0;
+        end
+    end
+
+    always @(*) begin
+      if (state == s1011)
+            out = 1;
+        else
+            out = 0;
+    end
+endmodule
