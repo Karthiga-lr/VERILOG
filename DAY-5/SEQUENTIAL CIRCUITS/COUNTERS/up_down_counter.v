61d4172afa4c1ea29684840604dfea34e1d67fe9
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-08 12:44:25 +0530

    Update up_down_counter.v

--- DAY-5/SEQUENTIAL CIRCUITS/COUNTERS/up_down_counter.v
+++ DAY-5/SEQUENTIAL CIRCUITS/COUNTERS/up_down_counter.v
@@ -1,1 +1,14 @@
-
+module up_down_counter(
+  input clk,
+  input rst, 
+  input d,
+  output reg[3:0] count);
+  always @(posedge clk or posedge rst)begin
+    if(rst)
+      count <= 4'b0;
+    else if (d)
+      count <= count+1;
+    else 
+      count <= count-1;
+  end
+endmodule
