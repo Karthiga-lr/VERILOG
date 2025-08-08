907894ea48adf219a9d9f8cc160f361b7eb451fc
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-08 12:48:57 +0530

    Update up_down_counter_tb.v

--- DAY-5/SEQUENTIAL CIRCUITS/COUNTERS/up_down_counter_tb.v
+++ DAY-5/SEQUENTIAL CIRCUITS/COUNTERS/up_down_counter_tb.v
@@ -1,1 +1,18 @@
-
+module up_down_counter_tb;
+  reg clk,rst,d;
+  wire [3:0] count;
+  up_down_counter dut (.clk(clk),.rst(rst),.d(d),.count(count));
+  always #5 clk = ~clk;
+  initial begin
+    $dumpfile("test.vcd");
+    $dumpvars(1);
+    $monitor("clk = %0d rst = %0d d = %0d count = %0d", clk,rst,d,count);
+    clk = 0;
+    rst = 1;
+    d = 0;
+    #10 rst = 0;
+    d = 1;  // count up
+    #20 d = 0; // count down
+    #100 $finish;
+  end
+endmodule
